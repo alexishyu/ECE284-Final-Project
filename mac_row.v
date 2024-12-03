578e93e2e1c4d0e1e768ecba08de48ef7bbce728
Jinshi He <soukangc@Jinshis-MacBook-Pro.local>
2024-12-02 16:30:38 -0800

    update mac structures

--- mac_row.v
+++ mac_row.v
@@ -1,5 +1,3 @@
-// Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
-// Please do not spread this code without permission 
 module mac_row (clk, out_s, in_w, in_n, valid, inst_w, reset);
 
   parameter bw = 4;
@@ -9,25 +7,31 @@ module mac_row (clk, out_s, in_w, in_n, valid, inst_w, reset);
   input  clk, reset;
   output [psum_bw*col-1:0] out_s;
   output [col-1:0] valid;
-  input  [bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
+  input  [bw-1:0] in_w; 
   input  [1:0] inst_w;
   input  [psum_bw*col-1:0] in_n;
 
-  wire  [(col+1)*bw-1:0] temp;
+  wire  [(col+1)*bw-1:0] temp; // Internal connections for weights
+  wire  [2*col-1:0] inst_temp; // Internal connections for control signals
+  wire  [psum_bw*col-1:0] out_temp; // Intermediate psum connections
+  wire  [col-1:0] valid_temp; // Valid signal for each tile
 
-  assign temp[bw-1:0]   = in_w;
+  assign temp[bw-1:0] = in_w; // Pass initial weight to the first tile
+  assign out_s = out_temp; // Connect intermediate outputs to the final output
+  assign valid = valid_temp; // Pass validity signals to the output
 
   genvar i;
-  for (i=1; i < col+1 ; i=i+1) begin : col_num
+  for (i = 1; i < col + 1; i = i + 1) begin : col_num
       mac_tile #(.bw(bw), .psum_bw(psum_bw)) mac_tile_instance (
          .clk(clk),
          .reset(reset),
-	 .in_w( temp[bw*i-1:bw*(i-1)]),
-	 .out_e(temp[bw*(i+1)-1:bw*i]),
-	 .inst_w(...),
-	 .inst_e(...),
-	 .in_n(...),
-	 .out_s(...));
+         .in_w(temp[bw*i-1:bw*(i-1)]),
+         .out_e(temp[bw*(i+1)-1:bw*i]),
+         .inst_w(inst_w),
+         .inst_e(inst_temp[2*i-1:2*(i-1)]),
+         .in_n(in_n[psum_bw*i-1:psum_bw*(i-1)]),
+         .out_s(out_temp[psum_bw*i-1:psum_bw*(i-1)])
+      );
   end
 
 endmodule
--- mac_tile.v
+++ mac_tile.v
@@ -1,30 +1,47 @@
-// Created by prof. Mingu Kang @VVIP Lab in UCSD ECE department
-// Please do not spread this code without permission 
 module mac_tile (clk, out_s, in_w, out_e, in_n, inst_w, inst_e, reset);
 
-parameter bw = 4;
-parameter psum_bw = 16;
+  parameter bw = 4;
+  parameter psum_bw = 16;
 
-output [psum_bw-1:0] out_s;
-input  [bw-1:0] in_w; // inst[1]:execute, inst[0]: kernel loading
-output [bw-1:0] out_e; 
-input  [1:0] inst_w;
-output [1:0] inst_e;
-input  [psum_bw-1:0] in_n;
-input  clk;
-input  reset;
+  output [psum_bw-1:0] out_s;
+  input  [bw-1:0] in_w; 
+  output [bw-1:0] out_e; 
+  input  [1:0] inst_w;
+  output [1:0] inst_e;
+  input  [psum_bw-1:0] in_n;
+  input  clk;
+  input  reset;
 
-...
-...
+  reg [bw-1:0] a_q; // Weight register
+  reg [1:0] inst_q; // Instruction register
 
-mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance (
+  wire [psum_bw-1:0] mac_out;
+
+  // MAC operation
+  mac #(.bw(bw), .psum_bw(psum_bw)) mac_instance (
         .a(a_q), 
-        .b(b_q),
-        .c(c_q),
-	.out(mac_out)
-); 
+        .b(in_w),
+        .c(in_n), // Use incoming partial sum directly
+        .out(mac_out)
+  ); 
 
-...
-...
+  always @(posedge clk or posedge reset) begin
+    if (reset) begin
+      a_q <= 0;
+      inst_q <= 0;
+    end else begin
+      inst_q <= inst_w; // Update instruction register
+      
+      if (inst_w[0]) begin
+        // Weight-loading phase: load the new weight into a_q
+        a_q <= in_w;
+      end
+      // No need for separate storage of partial sums
+    end
+  end
+
+  assign out_s = mac_out; // Output updated psum
+  assign out_e = in_w;    // Pass the input activation to the next tile
+  assign inst_e = inst_q; // Pass the instruction signal to the next tile
 
 endmodule
